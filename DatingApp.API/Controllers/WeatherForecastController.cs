19a56d12bf42a99718ea1c28fa510955dbd56180
unknown <payal.gaikwad@quest-global.com>
2020-07-25 17:23:17 +0530

    Register Login  Api

--- DatingApp.API/Controllers/WeatherForecastController.cs
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace DatingApp.API.Controllers
 {
+    
     [ApiController]
     [Route("[controller]")]
+    [Authorize]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
